e75d6a395588520481911b7d831dc253fd95dbd5
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-19 00:20:20 +0000

    Update .gitignore to ignore the instance directory, modify create_app to load config.py silently, and add instance/config.py for environment variable management.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=False)
+        app.config.from_pyfile('config.py', silent=True)
         pass
     else:
         app.config.from_mapping(test_config)
